aa86cb156c8bcbe8cf9c5d2fe1918aac906b3f18
wei.li <wei.li@zymobi.com>
2017-03-02 14:22:00 +0800

    侧边栏

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
+import SvgIcon from 'material-ui/SvgIcon';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+var injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
 
+const MenuIcon = (props) => (
+    <SvgIcon {...props}>
+        <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+    </SvgIcon>
+);
 const Header = React.createClass ({
-    PropTypes: {
-        admin: React.PropTypes.boolean
-    },
-    contextTypes: {
-        router: React.PropTypes.func.isRequired
+    getInitialState: function() {
+        return {open: false};
     },
     style: {
         appbar: {
-            position: "fixed", top: 0, zIndex: 1400
+            position: "fixed", top: 0, zIndex: 1200
         },
         link: {
             textDecoration: "none"
@@ -21,9 +28,16 @@ const Header = React.createClass ({
             marginRight: 10, marginTop: 5,
             color: 'rgba(0, 0, 0, 0.87)'
         }
+
+    },
+    handleToggle:function(){
+        this.setState({open: !this.state.open})
+    },
+    handleClose :function () {
+         this.setState({open: false})
     },
     render(){
-        // let title='会喝水的鱼';
+         // let title='会喝水的鱼';
         if (this.props.admin) {
             title = "Blog   Admin   Panel";
 
@@ -32,23 +46,37 @@ const Header = React.createClass ({
             this.style.appbar.background = "rgb(0,188,212)"
         }
         return(
+            <div>
             <AppBar
                 style={this.style.appbar}
+                iconElementLeft={
+                    <IconButton  onTouchTap={this.handleToggle} >
+                        <MenuIcon />
+                    </IconButton>
+
+                }
                 iconElementRight={
-                    <div> {(() => {
-                            return (<div>
                                 <IconButton
                                     iconStyle={{color: "white"}}
                                     iconClassName="muidocs-icon-custom-github"
                                     href="https://github.com/lwcmgowl/react"
                                     linkButton={true}
                                 />
-                            </div>);
-                    })()}</div>
                 }
+
                 zDepth={0}
             />
-
+            <Drawer
+                docked={false}
+                width={260}
+                open={this.state.open}
+                style={{zindex:1300}}
+                onRequestChange={(open) => this.setState({open})}
+                    >
+                    <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
+                    <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+            </Drawer>
+            </div>
         )
 
     }
--- app/main.js
+++ app/main.js
@@ -2,12 +2,11 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Header from './components/Header';
-// import DrawerUndockedExample from './components/Menu'
+
 
 const App = () => (
     <MuiThemeProvider>
         <Header />
-        {/*<DrawerUndockedExample/>*/}
     </MuiThemeProvider>
 );
 ReactDom.render(
